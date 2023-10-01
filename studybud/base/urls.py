10d22c149da2ef6246a6f15f4a58686fb7db1df8
Artiom <@H20CH3PO2>
2023-10-01 19:12:02 +0300

    test .gitignore

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name='room'),
 
     path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -29,3 +29,15 @@ def createRoom(request):
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)

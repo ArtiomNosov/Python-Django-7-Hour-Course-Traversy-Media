15089a121439900f76cb41692a74eac6dc1f17da
Artiom <@H20CH3PO2>
2023-10-01 19:52:20 +0300

    Add navbar features search on description and nome of topic

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -10,7 +11,11 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
 
     topics = Topic.objects.all()
 

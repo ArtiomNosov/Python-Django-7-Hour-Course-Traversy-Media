32b683335fb8a9821b0378a079be1beb7732f703
Artiom <@H20CH3PO2>
2023-10-01 18:01:50 +0300

    add models Topic, Room, Message

--- studybud/base/admin.py
+++ studybud/base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Topic, Room, Message
+
+admin.site.register(Topic)
+admin.site.register(Room)
+admin.site.register(Message)
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id':0, 'name':'Lets learn Python!'},
-    {'id':1, 'name':'Design with me!'},
-    {'id':2, 'name':'Fronend developers'}
-]
+# rooms = [
+#     {'id':0, 'name':'Lets learn Python!'},
+#     {'id':1, 'name':'Design with me!'},
+#     {'id':2, 'name':'Fronend developers'}
+# ]
+
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     
     context = {'room': room}
     return render(request, 'base/room.html', context)

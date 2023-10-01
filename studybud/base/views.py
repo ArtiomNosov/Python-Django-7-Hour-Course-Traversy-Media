a101f793bc2921fe3e2d49c1c96f8644ccd69eef
Artiom <@H20CH3PO2>
2023-10-01 19:36:01 +0300

    Add Browse Topics

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # rooms = [
@@ -10,7 +10,10 @@ from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
-    context = {'rooms': rooms}
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
